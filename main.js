8b4d47f9886645efc507bce5a622977feab2ff72
AnahatChahal <82654399+AnahatChahal@users.noreply.github.com>
2021-06-07 18:45:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status="";
+objects=[];
 function setup()
 {
     canvas=createCanvas(450,400);
@@ -18,4 +20,31 @@ function start()
 function modelLoaded()
 {
     console.log("model is loaded");
+    status=true;
+}
+
+function draw()
+{
+    image(video,0,0,450,400);
+    if(status !=""){
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++) {
+             document.getElementById("status").innerHTML = "Status : Objects Detected";  
+             fill("#FF0000"); 
+             percent = floor(objects[i].confidence * 100);
+              text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15); 
+              noFill(); 
+              stroke("#FF0000"); 
+              rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+            } 
+        } 
+    }
+
+function gotResult(error,results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
